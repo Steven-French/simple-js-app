02b7cbac8a504eae2854a94ea4d7e5eb0f83a9e0
Steven-French <93749446+Steven-French@users.noreply.github.com>
2024-01-12 11:31:48 -0700

    removed 1.2 comment

--- js/script.js
+++ js/script.js
@@ -2,27 +2,3 @@ let favoriteFood = 'Spaghetti';
 
 alert("Hello world");
 document.write(favoriteFood);
-
-
-/* let pokemonList = [
-    { 
-        name: "Bulbasaur",
-        height: 7,
-        types: ['grass', 'poison']
-    },
-
-    { 
-        name: "Ivysaur",
-        height: 10,
-        types: ['grass', 'poison']
-    },
-
-    { 
-        name: "Venusaur",
-        height: 20,
-        types: ['grass', 'poison']
-    },
-
-]
-
-*/
