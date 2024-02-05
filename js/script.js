da9011d2ada7908af5997ffd3d7afdb3ac002584
Steven-French <93749446+Steven-French@users.noreply.github.com>
2024-02-05 11:29:41 -0700

    Added for loop to Display Names and Heights
    Displays name and height of each pokemon in pokemonList. Displays "Wow, that's big!" for the largest one.

--- js/script.js
+++ js/script.js
@@ -18,3 +18,19 @@ let pokemonList = [
     },
 
 ]
+
+
+//loop to write name and height of every pokemon and declare which is big
+for (let i = 0; i <= pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") ")
+
+
+    if (pokemonList[i].height >= 20){
+        document.write("Wow, that's big!")
+    }
+        else { //do nothing
+    }
+
+    document.write("<br>")
+
+}
